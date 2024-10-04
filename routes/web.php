97e0749da6f462ac04b0a2adfd1021596b6118eb
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-10-04 17:10:17 +0200

    A medio Paso 6

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 Route::view('contacto', 'contact')->name('contact');
-Route::view('blog', 'blog')->name('blog');
+Route::get('blog', function () {
+    $posts = [
+        ['title' => 'Post 1'],
+        ['title' => 'Post 2'],
+        ['title' => 'Post 3'],
+        ['title' => 'Post 4'],
+    ];
+
+    return view('blog', compact('posts'));
+})->name('blog');
 Route::view('nosotros', 'about')->name('about');
 
 Route::get('/dashboard', function () {
