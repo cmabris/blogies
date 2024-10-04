0604888d6e2814f6da2fa72956b214d2fc23e5b2
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-10-04 15:46:03 +0200

    Fin Paso 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,12 @@
         <title>Inicio</title>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('blog') }}">Blog</a></li>
+        <li><a href="{{ route('about') }}">Nosotros</a></li>
+        <li><a href="{{ route('contact') }}">Contacto</a></li>
+    </ul>
         <h1>Inicio</h1>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-Route::view('contacto', 'contact');
-Route::view('blog', 'blog');
-Route::view('nosotros', 'about');
+Route::view('/', 'welcome')->name('home');
+Route::view('contacto', 'contact')->name('contact');
+Route::view('blog', 'blog')->name('blog');
+Route::view('nosotros', 'about')->name('about');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
