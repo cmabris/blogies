3a6feb1f2bc09444e02160aee4e53a6043f06a5d
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-10-09 16:41:52 +0200

    Paso 9 a medio

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = DB::table('posts')->get();
+        $posts = Post::all();
 
         return view('blog', compact('posts'));
     }
