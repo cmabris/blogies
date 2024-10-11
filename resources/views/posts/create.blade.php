ce1ebb5817ed359323c6422b981717d4e485a98f
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-10-11 16:16:55 +0200

    Fin paso 14

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,20 +1,24 @@
 <x-layout meta-title="Create a new Post" meta-description="Form to create a new Post">
     <h1>Create a new Post</h1>
 
-    @foreach($errors->all() as $error)
-        <p>{{ $error }}</p>
-    @endforeach
-
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
         <label>
             Title <br />
-            <input type="text" name="title">
+            <input type="text" name="title" value="{{ old('title') }}">
+            @error('title')
+            <br />
+            <small style="color: red">{{ $message }}</small>
+            @enderror
         </label>
         <br />
         <label>
             Body <br />
-            <textarea name="body"></textarea>
+            <textarea name="body">{{ old('body') }}</textarea>
+            @error('body')
+            <br />
+            <small style="color: red">{{ $message }}</small>
+            @enderror
         </label>
         <br />
         <button type="submit">Send</button>
