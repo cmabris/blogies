8be4d4c3c2f7e5fbfeb61cfa53259b177bcff630
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-10-16 16:25:45 +0200

    Fin Paso 22

--- resources/views/components/partials/navigation.blade.php
+++ resources/views/components/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <ul>
-    <li><a href="{{ route('home') }}">Home</a></li>
-    <li><a href="{{ route('posts.index') }}">Blog</a></li>
-    <li><a href="{{ route('about') }}">Nosotros</a></li>
-    <li><a href="{{ route('contact') }}">Contacto</a></li>
+    <li><a class="{{ request()->routeIs('home') ? 'text-green-400' : 'text-gray-700'}}" href="{{ route('home') }}">Home</a></li>
+    <li><a class="{{ request()->routeIs('posts.*') ? 'text-green-400' : 'text-gray-700' }}" href="{{ route('posts.index') }}">Blog</a></li>
+    <li><a class="{{ request()->routeIs('about') ? 'text-green-400' : 'text-gray-700' }}" href="{{ route('about') }}">Nosotros</a></li>
+    <li><a class="{{ request()->routeIs('contact') ? 'text-green-400' : 'text-gray-700' }}" href="{{ route('contact') }}">Contacto</a></li>
 </ul>
