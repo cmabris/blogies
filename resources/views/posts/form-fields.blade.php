3a47a66c1ce31e790e4d6e07564ef21bbffe63a8
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-10-18 17:10:50 +0200

    A medio paso 28

--- resources/views/posts/form-fields.blade.php
+++ resources/views/posts/form-fields.blade.php
@@ -4,15 +4,15 @@
                   name="title"
                   type="text"
                   value="{{ old('title', $post->title) }}"
-                  class="w-full mt-1"
+                  class="block w-full mt-1"
     />
-    <x-input-error :messages="$errors->get('title')" />
+    <x-input-error :messages="$errors->get('title')" class="mt-2" />
 </div>
 <div>
     <x-input-label for="body" :value="__('Body')" />
     <x-textarea id="body"
                 name="body"
-                class="w-full mt-1"
+                class="block w-full mt-1"
     >{{ old('body', $post->body) }}</x-textarea>
-    <x-input-error :messages="$errors->get('body')" />
+    <x-input-error :messages="$errors->get('body')" class="mt-2" />
 </div>
